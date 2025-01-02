a0ed4598fe94371b96818e13387dabba5abe20bb
Bagas N <nuriksabagas@gmail.com>
2025-01-03 04:02:48 +0700

    feat: add 1.2.hello_world.cpp

---
+++ 01.hello_world/1.2.hello_world.cpp
@@ -0,0 +1,14 @@
+// This program outputs the message "Hello, World!" to the monitor
+
+/*
+ * Reference on how to compile using clang:
+ * https://0xstubs.org/using-the-c23-std-module-with-clang-18/
+ */
+
+import std; // gain access to the C++ standard library
+
+int main() // C++ programs start by executing the function main
+{
+  std::cout << "Hello, World!\n"; // output "Hello, World!"
+  return 0;
+}
